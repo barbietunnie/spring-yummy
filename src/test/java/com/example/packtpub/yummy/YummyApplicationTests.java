38ecddb7892a1facd1fdaa3ff6d3d3d6e7e6031e
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-08-15 10:02:39 +0100

    Perform some testing

--- src/test/java/com/example/packtpub/yummy/YummyApplicationTests.java
+++ src/test/java/com/example/packtpub/yummy/YummyApplicationTests.java
@@ -2,15 +2,31 @@ package com.example.packtpub.yummy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class YummyApplicationTests {
+	@Autowired
+	DatePrinter datePrinter;
+	@MockBean
+	TimeFactory timeFactory;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void datePrinterWorks() {
+		given(timeFactory.now()).willReturn(LocalDateTime.of(2017, 8,15, 9, 50, 27));
+		assertEquals("Now, it is 2017-08-15T09:50:27", datePrinter.printDate());
+	}
 }
