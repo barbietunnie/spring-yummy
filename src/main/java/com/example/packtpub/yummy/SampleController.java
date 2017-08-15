104a181efa57da9a9a195983c5464d31c7fa3fd6
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-08-15 11:43:23 +0100

    Use @RestController instead of @Controller for the Controller

--- src/main/java/com/example/packtpub/yummy/SampleController.java
+++ src/main/java/com/example/packtpub/yummy/SampleController.java
@@ -3,14 +3,15 @@ package com.example.packtpub.yummy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@Controller
+@RestController
 @RequestMapping
 public class SampleController {
     @RequestMapping
-    public @ResponseBody String sayTheTime() {
+    public String sayTheTime() {
         return LocalDateTime.now().toString();
     }
 }
