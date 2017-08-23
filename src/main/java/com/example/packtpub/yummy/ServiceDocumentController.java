822175d26bcf67247ca649b989f803baef1525ba
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-08-23 10:09:53 +0100

    Add HATEOAS and test it

---
+++ src/main/java/com/example/packtpub/yummy/ServiceDocumentController.java
@@ -0,0 +1,24 @@
+package com.example.packtpub.yummy;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.BasicLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping
+public class ServiceDocumentController {
+    @GetMapping("/")
+    public Resource<String> getServiceDocument() {
+        return new Resource<>(
+                "Yummy is the best service",
+                BasicLinkBuilder.linkToCurrentMapping().withSelfRel(),
+                BasicLinkBuilder.linkToCurrentMapping().slash("bookmarks").withRel("bookmarks"),
+                linkTo(methodOn(BookmarksController.class).addBookmark(null)).withRel("bookmarks2")
+        );
+    }
+}
